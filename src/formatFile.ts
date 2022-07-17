e175d07d923b130a7931d545f4215425172b16e5
jerrywcy <jerrymonkeynj@163.com>
2022-07-17 10:30:22 +0800

    Reconstruct api.ts and formatFile.ts

--- src/formatFile.ts
+++ src/formatFile.ts
@@ -1,88 +1,81 @@
-import { parseYaml, stringifyYaml, TFile } from "obsidian";
-import VuepressPublisher from './main';
+import { parseYaml } from "obsidian";
+import {VuepressPublisherSettings} from './main';
 import { relative, dirname, basename, join } from "path";
-import { CloudHandler } from 'service/api';
-import { Failure, getAPI, Success } from "obsidian-dataview";
+import { Failure, getAPI } from "obsidian-dataview";
 
 export class Formatter {
-    plugin: VuepressPublisher;
-    cloudHandler: CloudHandler
+    settings: VuepressPublisherSettings
 
-    constructor(plugin: VuepressPublisher) {
-        this.plugin = plugin;
-        this.cloudHandler = this.plugin.cloudHandler;
+    constructor(settings: VuepressPublisherSettings) {
+        this.settings = settings;
     }
-    async formatFile(file: TFile): Promise<string> {
-        let data = await app.vault.read(file);
-        data = await this.replaceDataView(data, file.path);
-        data = this.replaceAdmonition(data);
-        data = this.replaceChart(data);
-        data = this.replaceEChart(data);
-        data = this.replaceWikiLinks(data, file.path);
-        data = await this.replaceAssetsLinks(data, file.path);
-        return data;
+    async formatFile(content: string, filePath: string): Promise<string> {
+        content = await this.replacecontentView(content, filePath);
+        content = this.replaceAdmonition(content);
+        content = this.replaceChart(content);
+        content = this.replaceEChart(content);
+        content = this.replaceWikiLinks(content, filePath);
+        content = this.replaceAssetsLinks(content);
+        return content;
     }
 
-    replaceWikiLinks(data: string, filePath: string): string {
+    replaceWikiLinks(content: string, filePath: string): string {
         let wikiLinkPattern = /\[\[(?<path>[^|]*?)(?:\|(?<text>[^|]*?))?\]\]/gu;
 
-        for (let match of data.matchAll(wikiLinkPattern)) {
+        for (let match of content.matchAll(wikiLinkPattern)) {
             let { path, text } = match.groups;
             let linkPattern = /(?<link>(?:[^#]*))(?<title>#[^#]*)?/u;
             let { link, title } = path.match(linkPattern).groups;
             let toFile = app.metadataCache.getFirstLinkpathDest(link, filePath);
             let toFilePath = join(relative(filePath, dirname(toFile.path)), basename(filePath, ".md"));
-            data = data.replace(match[0], `[${text ? text : path}](${toFilePath + (title ?? "")})`);
+            content = content.replace(match[0], `[${text ? text : path}](${toFilePath + (title ?? "")})`);
         }
-        return data;
+        return content;
     }
 
-    async replaceAssetsLinks(data: string, filePath: string): Promise<string> {
+    replaceAssetsLinks(content: string): string {
         let linkPattern = /\[(?<text>[^\[\]]*?)\]\((?<link>[^\(\)]*?)\)/gu;
-        for (let match of data.matchAll(linkPattern)) {
+        for (let match of content.matchAll(linkPattern)) {
             let { text, link } = match.groups;
             if (link.match(/https?:\/\//u) === null) {
                 let asset = app.vault.getAbstractFileByPath(link);
-                if (!(asset instanceof TFile)) { continue; }
-                if (asset.stat.size > 1024 * 1024 * 10) { continue; }
-                let cloudPath = this.plugin.settings.assetsFolder + asset.name;
-                await this.cloudHandler.updateFile(asset, cloudPath);
+                let cloudPath = this.settings.assetsFolder + asset.name;
                 link = cloudPath;
             }
-            data.replace(match[0], `[${text ?? ""}](${link})`);
+            content.replace(match[0], `[${text ?? ""}](${link})`);
         }
-        return data;
+        return content;
     }
 
-    async replaceDataView(data: string, filePath: string): Promise<string> {
+    async replacecontentView(content: string, filePath: string): Promise<string> {
         let dv = getAPI();
-        if (dv === undefined) return data;
-        let dataviewPattern = /(?<separator>`*)dataview\n(?<content>[\s\S]*)\n?\k<separator>/gu;
-        for (let match of data.matchAll(dataviewPattern)) {
+        if (dv === undefined) return content;
+        let contentviewPattern = /(?<separator>`*)dataview\n(?<content>[\s\S]*)\n?\k<separator>/gu;
+        for (let match of content.matchAll(contentviewPattern)) {
             let { separator, content } = match.groups;
             let res = await dv.queryMarkdown(content);
             if (res.successful) {
-                data = data.replace(match[0], res.value);
+                content = content.replace(match[0], res.value);
             }
             else {
-                data = data.replace(match[0], (res as Failure<string, string>).error);
+                content = content.replace(match[0], (res as Failure<string, string>).error);
             }
         }
-        let dataviewJSPattern = /(?<separator>`*)dataviewjs\n(?<content>[\s\S]*)\n?\k<separator>/gu;
-        for (let match of data.matchAll(dataviewJSPattern)) {
+        let contentviewJSPattern = /(?<separator>`*)dataviewjs\n(?<content>[\s\S]*)\n?\k<separator>/gu;
+        for (let match of content.matchAll(contentviewJSPattern)) {
             let { separator, content } = match.groups;
             let res = await dv.queryMarkdown(content);
             if (res.successful) {
-                data = data.replace(match[0], res.value);
+                content = content.replace(match[0], res.value);
             }
             else {
-                data = data.replace(match[0], (res as Failure<string, string>).error);
+                content = content.replace(match[0], (res as Failure<string, string>).error);
             }
         }
-        return data;
+        return content;
     }
 
-    replaceAdmonition(data: string): string {
+    replaceAdmonition(content: string): string {
         let admonitionToVuepress = new Map<string, string>();
         admonitionToVuepress.set("tip", "tip");
         admonitionToVuepress.set("hint", "tip");
@@ -96,23 +89,23 @@ export class Formatter {
         admonitionToVuepress.set("error", "danger");
         admonitionToVuepress.set("note", "note");
         let admonitionPattern = /(?<separator>`*)ad-(?<type>[\S]+)\n(?:title: (?<title>.+))?\n(?<content>[\s\S]*)\n?\k<separator>/gu;
-        for (let match of data.matchAll(admonitionPattern)) {
+        for (let match of content.matchAll(admonitionPattern)) {
             let { separator, type, title, content } = match.groups;
             let containerType = admonitionToVuepress.get(type);
             if (type !== undefined) {
-                data = data.replace(match[0], `::: ${containerType} ${title ?? ""}
+                content = content.replace(match[0], `::: ${containerType} ${title ?? ""}
 ${content}
 :::`);
             }
         }
-        return data;
+        return content;
     }
 
-    replaceChart(data: string): string {
+    replaceChart(content: string): string {
         let chartPattern = /(?<separator>`*)chart\n(?<content>[\s\S]*)\n?\k<separator>/gu;
-        for (let match of data.matchAll(chartPattern)) {
+        for (let match of content.matchAll(chartPattern)) {
             let { separator, content } = match.groups;
-            data = data.replace(match[0], `::: chart
+            content = content.replace(match[0], `::: chart
 
 \`\`\`json
 ${JSON.stringify(parseYaml(content), null, 4)}
@@ -121,11 +114,11 @@ ${JSON.stringify(parseYaml(content), null, 4)}
 :::
 `)
         }
-        return data;
+        return content;
     }
 
-    replaceEChart(data: string): string {
-        return data;
+    replaceEChart(content: string): string {
+        return content;
     }
 
 }
