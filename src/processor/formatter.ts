35fcf50d5dd93d77b7174d662e5743001e8d8835
jerrywcy <jerrymonkeynj@163.com>
2022-07-18 23:36:26 +0800

    Reconstruct formatter.ts

--- src/processor/formatter.ts
+++ src/processor/formatter.ts
@@ -1,105 +1,172 @@
 import { VuepressPublisherSettings } from './../main';
-import { Failure, DataviewApi } from "obsidian-dataview";
-import path from "path";
+import { Failure, DataviewApi, getAPI } from 'obsidian-dataview';
+import { posix } from 'path';
+
+class Content {
+    content: string;
+    sourcePath: string;
+
+    constructor(content: string, sourcePath: string = '') {
+        this.content = content;
+        this.sourcePath = sourcePath;
+    }
+
+    replaceExtended(regex: RegExp, replace: (match: RegExpMatchArray) => string): this {
+        replace = replace.bind(this);
+        for (let match of this.content.matchAll(regex)) {
+            this.content = this.content.replace(match[0], replace(match));
+        }
+        return this;
+    }
+}
+
+function isMarkdown(filePath: string) {
+    return posix.extname(filePath) === '' || posix.extname(filePath) === '.md';
+}
 
 export default class Formatter {
-  constructor(public settings: VuepressPublisherSettings){}
+    constructor(public settings: VuepressPublisherSettings) {}
 
-  replaceWikiLinks(content: string, getFirstLinkpathDest: (linkpath: string, sourcePath: string) => string | null): string {
-    const wikiLinkPattern = /\[\[(?<filePath>[^|]*?)(?:\|(?<text>[^|]*?))?\]\]/gu;
-    for (const match of content.matchAll(wikiLinkPattern)) {
-      const { filePath, text } = match.groups;
-      const linkPattern = /(?<link>(?:[^#]*))(?<title>#[^#]*)?/u;
-      const { link, title } = filePath.match(linkPattern).groups;
-      const toFile = getFirstLinkpathDest(link, filePath)
-      const toFilePath = path.relative(toFile, this.settings.publishFolder)
-      content = content.replace(match[0], `[${text ? text : path}](${toFilePath + (title ?? "")})`);
+    async format(content: string, filePath: string) {
+        content = this.replaceLink(content, filePath);
+        content = this.replaceAdmonition(content);
+        content = await this.replaceDataview(content, getAPI());
     }
-    return content
-  }
 
-  replaceAssetsLinks(content: string): string {
-    const linkPattern = /\[(?<text>[^[\]]*?)\]\((?<link>[^()]*?)\)/gu;
-    for (const match of content.matchAll(linkPattern)) {
-      const { text } = match.groups;
-      let { link } = match.groups;
-      if (link.match(/https?:\/\//u) === null) {
-        const asset = app.vault.getAbstractFileByPath(link);
-        const cloudPath = "/" + asset.name;
-        link = cloudPath;
-      }
-      content.replace(match[0], `[${text ?? ""}](${link})`);
+    replaceLink(
+        content: string,
+        filePath: string = '',
+        getLink: (filePath: string, sourcePath: string) => string = (filePath: string, sourcePath: string) =>
+            app.metadataCache.getFirstLinkpathDest(filePath, sourcePath).path
+    ) {
+        let settings = this.settings;
+        return new Content(content, filePath)
+            .replaceExtended(
+                /(?<!!)\[(?<alias>[^\[\]]*)?\]\((?<filePath>[^\(\)]+)(?<subPath>#[^\|#\(\)]*)?\)/gu,
+                function (match: RegExpMatchArray) {
+                    let { filePath, subPath = '', alias = '' } = match.groups;
+                    if (!isMarkdown(filePath)) {
+                        filePath = posix.join('/', posix.basename(filePath));
+                        return `[${alias}](${filePath + subPath})`;
+                    } else return match[0];
+                }
+            )
+            .replaceExtended(
+                /!\[(?<alias>[^\[\]]*)?\]\((?<filePath>[^\(\)]+)(?<subPath>#[^\|#\(\)]*)?\)/gu,
+                function (match: RegExpMatchArray) {
+                    let { filePath, subPath = '', alias = '' } = match.groups;
+                    if (!isMarkdown(filePath)) {
+                        filePath = posix.join('/', posix.basename(filePath));
+                        return `![${alias}](${filePath + subPath})`;
+                    } else return match[0];
+                }
+            )
+            .replaceExtended(
+                /!\[\[(?<filePath>[^\|\[\]#]*)(?<subPath>#[^\|\[\]#]*)?(?:\|(?<alias>[^\|\[\]]*))?\]\]/gu,
+                function (match: RegExpMatchArray) {
+                    let { filePath, subPath = '', alias } = match.groups;
+                    if (isMarkdown(filePath)) {
+                        filePath = posix.join(settings.publishFolder, getLink(filePath, this.sourcePath));
+                        return `\n@include{${filePath}}\n`;
+                    } else {
+                        filePath = posix.join('/', posix.basename(filePath));
+                        alias = alias ?? posix.basename(filePath);
+                        if (alias.match(/^(?<width>\d+)(?:x(?<height>\d+))?$/u)) {
+                            let size = alias.includes('x') ? alias : alias + 'x';
+                            return `![${posix.basename(filePath)}](${filePath} =${
+                                alias.includes('x') ? alias : alias + 'x'
+                            })`;
+                        }
+                        return `![${alias}](${filePath + subPath})`;
+                    }
+                }
+            )
+            .replaceExtended(
+                /(?<!!)\[\[(?<filePath>[^\|#]*)(?<subPath>#[^\|#]*)?(?:\|(?<alias>[^\|]*))?\]\]/gu,
+                function (match: RegExpMatchArray) {
+                    let { filePath, subPath = '', alias } = match.groups;
+                    if (isMarkdown(filePath)) {
+                        alias = alias ?? filePath;
+                        filePath = posix.join(settings.publishFolder, getLink(filePath, this.sourcePath));
+                        return `[${alias}](${filePath + subPath})`;
+                    } else {
+                        filePath = posix.join('/', posix.basename(filePath));
+                        alias = alias ?? posix.basename(filePath);
+                        return `[${alias}](${filePath + subPath})`;
+                    }
+                }
+            ).content;
     }
-    return content;
-  }
 
-  replaceAdmonition(content: string): string {
-    const admonitionToVuepress = new Map<string, string>();
-    admonitionToVuepress.set("tip", "tip");
-    admonitionToVuepress.set("hint", "tip");
-    admonitionToVuepress.set("important", "tip");
-    admonitionToVuepress.set("info", "info");
-    admonitionToVuepress.set("todo", "info");
-    admonitionToVuepress.set("warning", "warning");
-    admonitionToVuepress.set("caution", "warning");
-    admonitionToVuepress.set("attention", "warning");
-    admonitionToVuepress.set("danger", "danger");
-    admonitionToVuepress.set("error", "danger");
-    admonitionToVuepress.set("note", "note");
-    const admonitionPattern = /(?<separator>`*)ad-(?<type>[\S]+)\n(?:title: (?<title>.+))?\n(?<content>[\s\S]*)\n?\k<separator>/gu;
-    for (const match of content.matchAll(admonitionPattern)) {
-      let { content } = match.groups;
-      const { type, title } = match.groups
-      const containerType = admonitionToVuepress.get(type);
-      if (type !== undefined) {
-        content = content.replace(match[0], `::: ${containerType} ${title ?? ""}
-${content}
-:::`);
-      }
+    replaceAdmonition(content: string): string {
+        const admonitionToVuepress = new Map<string, string>();
+        admonitionToVuepress.set('tip', 'tip');
+        admonitionToVuepress.set('hint', 'tip');
+        admonitionToVuepress.set('important', 'tip');
+        admonitionToVuepress.set('info', 'info');
+        admonitionToVuepress.set('todo', 'info');
+        admonitionToVuepress.set('warning', 'warning');
+        admonitionToVuepress.set('caution', 'warning');
+        admonitionToVuepress.set('attention', 'warning');
+        admonitionToVuepress.set('danger', 'danger');
+        admonitionToVuepress.set('error', 'danger');
+        admonitionToVuepress.set('note', 'note');
+        return new Content(content).replaceExtended(
+            /(```+)ad-(?<type>[\S]+)\n(?:title: (?<title>.+)\n)?(?<content>[\s\S]*)\n\1/gu,
+            function (match: RegExpMatchArray) {
+                let { separator, type, title = '', content } = match.groups;
+                type = admonitionToVuepress.get(type);
+                if (type === undefined) {
+                    return match[0];
+                } else {
+                    if (title !== '') title = ' ' + title;
+                    return `::: ${type}${title}\n${content}\n:::`;
+                }
+            }
+        ).content;
     }
-    return content;
-  }
 
-  async replaceContentView(content: string, dv: DataviewApi): Promise<string> {
-    if (dv === undefined) return content;
-    const contentviewPattern = /(?<separator>`*)dataview\n(?<content>[\s\S]*)\n?\k<separator>/gu;
-    for (const match of content.matchAll(contentviewPattern)) {
-      let { content } = match.groups;
-      const res = await dv.queryMarkdown(content);
-      if (res.successful) {
-        content = content.replace(match[0], res.value);
-      }
-      else {
-        content = content.replace(match[0], (res as Failure<string, string>).error);
-      }
+    async replaceDataview(content: string, dv: DataviewApi): Promise<string> {
+        if (dv === undefined) return content;
+        const contentviewPattern = /(?<separator>`*)dataview\n(?<content>[\s\S]*)\n?\k<separator>/gu;
+        for (const match of content.matchAll(contentviewPattern)) {
+            let { content } = match.groups;
+            const res = await dv.queryMarkdown(content);
+            if (res.successful) {
+                content = content.replace(match[0], res.value);
+            } else {
+                content = content.replace(match[0], (res as Failure<string, string>).error);
+            }
+        }
+        const contentviewJSPattern = /(?<separator>`*)dataviewjs\n(?<content>[\s\S]*)\n?\k<separator>/gu;
+        for (const match of content.matchAll(contentviewJSPattern)) {
+            let { content } = match.groups;
+            const res = await dv.queryMarkdown(content);
+            if (res.successful) {
+                content = content.replace(match[0], res.value);
+            } else {
+                content = content.replace(match[0], (res as Failure<string, string>).error);
+            }
+        }
+        return content;
     }
-    const contentviewJSPattern = /(?<separator>`*)dataviewjs\n(?<content>[\s\S]*)\n?\k<separator>/gu;
-    for (const match of content.matchAll(contentviewJSPattern)) {
-      let { content } = match.groups;
-      const res = await dv.queryMarkdown(content);
-      if (res.successful) {
-        content = content.replace(match[0], res.value);
-      }
-      else {
-        content = content.replace(match[0], (res as Failure<string, string>).error);
-      }
-    }
-    return content;
-  }
 
-  replaceChart(content: string, parseYaml: (yaml: string) => string): string {
-    const chartPattern = /(?<separator>`*)chart\n(?<content>[\s\S]*)\n?\k<separator>/gu;
-    for (const match of content.matchAll(chartPattern)) {
-      let { content } = match.groups;
-      content = content.replace(match[0], `::: chart
+    replaceChart(content: string, parseYaml: (yaml: string) => string): string {
+        const chartPattern = /(?<separator>`*)chart\n(?<content>[\s\S]*)\n?\k<separator>/gu;
+        for (const match of content.matchAll(chartPattern)) {
+            let { content } = match.groups;
+            content = content.replace(
+                match[0],
+                `::: chart
 
 \`\`\`json
 ${JSON.stringify(parseYaml(content), null, 4)}
 \`\`\`
 
 :::
-`)
+`
+            );
+        }
+        return content;
     }
-    return content;
-  }
 }
--- src/tests/formatter.test.ts
+++ src/tests/formatter.test.ts
@@ -1,59 +1,96 @@
-import Formatter from "../processor/formatter";
+import Formatter from '../processor/formatter';
 
 const settings = {
-  publishFolder: "docs",
-  publishKey: "publish",
-  githubRepo: "",
-  githubSSHKey: "",
-  githubVuepressConfigFile: "",
-  giteeVuepressConfigFile: ""
+    publishFolder: 'docs',
+    publishKey: 'publish',
+    githubRepo: '',
+    githubSSHKey: '',
+    githubVuepressConfigFile: '',
+    giteeVuepressConfigFile: '',
 };
 
-const formatter = new Formatter(settings)
-
+const formatter = new Formatter(settings);
 
 //wiki links
 //也有两种方式，一种是文件与文件间的相对路径，另一种是从docs出发的路径。我觉得第二种方式更好一点，ob能直接返回根路径，处理下就能用
-
-test("formatter assets links", ()=>{
-  const getFirstLinkpathDest = ()=>"obsidian/docs/fileFolder/file.md"
-
-  expect(formatter.replaceWikiLinks("[[file]]", getFirstLinkpathDest)).toBe("[file](docs/fileFolder/file.md)")
-  expect(formatter.replaceWikiLinks("#fewfwe\n- [[file]]\nfewf", getFirstLinkpathDest)).toBe("#fewfwe\n- [file](docs/fileFolder/file.md)fewf")
-})
 //assets links
 //vuepress图片路径和config里的base,dirFolder有关，如docs/assets/image.png, 如果base为/obsidian, 则路径为[image](/base/dirFolder/assets/image.png),
 //我们默认为最短路径,所以图片默认都传到.vuepress/public路径下，暂时不支持自定义
 
-test("formatter assets links", ()=>{
+describe('Links', () => {
+    const getLink = () => 'fileFolder/file.md';
 
-  expect(formatter.replaceAssetsLinks(`![[test.png]]`)).toBe(`[test](/test.png)`)
-  expect(formatter.replaceAssetsLinks(`![[../../assets/test.png]]`)).toBe(`[test](/test.png)`)
-  expect(formatter.replaceAssetsLinks(`![](assets/test.png)`)).toBe(`[test](/test.png)`)
-})
+    describe('Normal Links', () => {
+        test('Normal link to some other page', () => {
+            expect(formatter.replaceLink('[file](file)')).toBe('[file](file)');
+        });
 
+        test('Normal link to assets', () => {
+            expect(formatter.replaceLink('[img](image.png)')).toBe('[img](/image.png)');
+        });
+
+        test('Embed link', () => {
+            expect(formatter.replaceLink(`![](test.png)`)).toBe(`![](/test.png)`);
+            expect(formatter.replaceLink(`![](assets/test.png)`)).toBe(`![](/test.png)`);
+        });
+    });
+
+    describe('Bidirectional Links and embeds', () => {
+        test('Normal bidirectional links', () => {
+            expect(formatter.replaceLink('[[file]]', '1.md', getLink)).toBe('[file](docs/fileFolder/file.md)');
+        });
+
+        test('Bidirectional Link to Assets', () => {
+            expect(formatter.replaceLink('[[image.png]]')).toBe('[image.png](/image.png)');
+        });
+
+        test('Bidirectional links within contents', () => {
+            expect(formatter.replaceLink('#fewfwe\n- [[file]]\nfewf', '1.md', getLink)).toBe(
+                '#fewfwe\n- [file](docs/fileFolder/file.md)\nfewf'
+            );
+        });
+
+        test('Embed assets', () => {
+            expect(formatter.replaceLink(`![[test.png]]`)).toBe(`![test.png](/test.png)`);
+            expect(formatter.replaceLink(`![[../../assets/test.png]]`)).toBe(`![test.png](/test.png)`);
+        });
+
+        test('Embed asset with alias', () => {
+            expect(formatter.replaceLink(`![[test.png|alias]]`)).toBe(`![alias](/test.png)`);
+        });
+
+        test('Embed asset with resizing', () => {
+            expect(formatter.replaceLink(`![[test.png|200]]`)).toBe(`![test.png](/test.png =200x)`);
+            expect(formatter.replaceLink(`![[test.png|200x300]]`)).toBe(`![test.png](/test.png =200x300)`);
+        });
+    });
+});
 
 //admonition
 //两种类型，一种有标题，一种没标题
-test("formatter admonition", () => {
-  const admonitionInfo = `\`\`\`ad-info
-  title: testTitle
-  test
-  \`\`\``
 
-  const admonitionTip = `\`\`\`ad-tip
-  test
-  \`\`\``
+describe('Admonition', () => {
+    test('Admonition without title', () => {
+        const admonitionTip = `\`\`\`ad-tip
+test
+\`\`\``;
+        expect(formatter.replaceAdmonition(admonitionTip)).toBe(`::: tip
+test
+:::`);
+    });
 
-  expect(formatter.replaceAdmonition(admonitionInfo)).toBe(`::: info testTitle
-  test
-  :::
-  `)
-
-  expect(formatter.replaceAdmonition(admonitionTip)).toBe(`::: tip
-  test
-  :::
-  `)
-})
+    test('Admonition with title', () => {
+        const admonitionInfo = `\`\`\`ad-info
+title: testTitle
+test
+\`\`\``;
+        expect(formatter.replaceAdmonition(admonitionInfo)).toBe(`::: info testTitle
+test
+:::`);
+    });
+});
 
 //chart
+describe('Chart', () => {});
+
+describe('Dataview', () => {});
